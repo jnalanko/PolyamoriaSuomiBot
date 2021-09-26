43ab5c32311d1434028c3d757a9a5d1f6c7a0ca5
Jarno N. Alanko <alanko.jarno@gmail.com>
2021-09-26 19:18:06 +0300

    Different intervals for different channels

--- bot.py
+++ bot.py
@@ -8,9 +8,9 @@ from multiprocessing                import Process
 from datetime import datetime, timedelta
 
 class AutoDeleteCallBack:
-    async def run(self, client, delete_older_than_minutes, active_channel_names):
+    async def run(self, client, delete_older_than_minutes, channel_name):
         for channel in client.get_all_channels():
-            if channel.name in active_channel_names:
+            if channel.name == channel_name:
                 prev_time = datetime.utcnow() - timedelta(minutes=delete_older_than_minutes)
                 async for elem in channel.history(before = prev_time):
                     await elem.delete()
@@ -26,9 +26,9 @@ sched = AsyncIOScheduler()
 print("Loading config")
 config = yaml.safe_load(open("config_local.yaml"))
 token = config["token"]
-callback_interval = config["callback_interval_minutes"]
-delete_older_than_minutes = config["delete_older_than_minutes"]
-active_channel_names = config["active_channels"]
+
+autodel_config = config["autodelete_channels"] # Dicts {channel: X, callback_interval_minutes: Y, delete_older_than_minutes: Z]
+#print(X["channel"], X["callback_interval_minutes"], X["delete_older_than_minutes"])
 
 print("Deleting every {} minutes all messages older than {} minutes from these channels: {}".format(callback_interval, delete_older_than_minutes, str(active_channel_names)))
 
@@ -47,7 +47,8 @@ async def on_ready():
     else: this.running = True
 
     print("Logged in. Setting up callbacks.", flush=True)
-    sched.add_job(autodelete.run, 'interval', (client, delete_older_than_minutes, active_channel_names), minutes=callback_interval)
+    for job in autodel_config:
+        sched.add_job(autodelete.run, 'interval', (client, X["delete_older_than_minutes"], X["channel"], minutes=X["callback_interval_minutes"])
     sched.start()
     print(f"Setup finished. Running.", flush=True)
     for channel in client.get_all_channels():

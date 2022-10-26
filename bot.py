deeafe792bb816e8a43456d20a48ee33d57ea95a
Jarno N. Alanko <alanko.jarno@gmail.com>
2022-10-26 23:44:25 +0300

    Negative numbers and whitespace

--- bot.py
+++ bot.py
@@ -270,18 +270,24 @@ def do_roll(expression):
     rolls = []
     sum_of_constants = 0
 
+    expression = expression.replace("+", " + ").replace("-", " - ")
+
+    sign = 1 # +1 or -1
+
     try: 
         tokens = expression.split()
         if len(tokens) == 0:
             return "Anna heitto muodossa 2d6 + 5"
         for i, token in enumerate(tokens):
+            token = token.strip()
             if i % 2 == 1:
-                if token != '+':
-                    raise ValueError("Syntaksivirhe: " + token)
+                if token == '+': sign = 1
+                elif token == '-': sign = -1
+                else: raise ValueError("Syntaksivirhe: " + token)
             else:
                 if token.count("d") == 0:
                     # Constant
-                    sum_of_constants += int(to_positive_integer(token, 1e6))
+                    sum_of_constants += int(to_positive_integer(token, 1e6)) * sign
                 elif token.count("d") == 1:
                     # Dice
                     n_dice, n_sides = token.split('d')
@@ -289,13 +295,13 @@ def do_roll(expression):
                     n_dice = to_positive_integer(n_dice, 100)
                     n_sides = to_positive_integer(n_sides, 1e6)
                     while n_dice > 0:
-                        rolls.append(random.randint(1,n_sides))
+                        rolls.append(random.randint(1,n_sides) * sign)
                         n_dice -= 1
                 else:
                     raise ValueError("Syntaksivirhe: " + token)
     except ValueError as e: return str(e)
 
-    if sum_of_constants > 0: 
+    if sum_of_constants != 0: 
         message = "{} + {} = {}".format(rolls, sum_of_constants, sum(rolls) + sum_of_constants)
     else:
         message = "{} = {}".format(rolls, sum(rolls))

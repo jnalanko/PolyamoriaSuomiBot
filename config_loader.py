772e8455d196d60c0b025695837f173b31eb3316
Tomi Javanainen <tomi.javanainen@gmail.com>
2023-11-01 13:01:47 +0200

    Move db_host, db_password, db_user to common config. Use env vars for these if exist.

--- config_loader.py
+++ config_loader.py
@@ -1,3 +1,4 @@
+import os
 from dataclasses import dataclass
 from typing import Dict
 
@@ -12,20 +13,21 @@ class InstanceConfig:
     admin_user_id: int
     bot_channel_id: int
     midnight_channel_id: int
-    db_host: str
     db_name: str
-    db_password: str
-    db_user: str
 
 
 @dataclass
 class Config:
     instances: Dict[int, InstanceConfig]
+    db_host: str
+    db_password: str
+    db_user: str
     token: str
     master_admin_user_id: int
 
 
 def load_config() -> Config:
+    """Environment variables override db_host, db_password and db_user"""
     config_dict = yaml.safe_load(open(LOCAL_SETTINGS_FILENAME))
     instance_configs = {}
     for _id, values in config_dict["instances"].items():
@@ -33,10 +35,9 @@ def load_config() -> Config:
 
     return Config(
         instances=instance_configs,
+        db_host=os.getenv("DATABASE_HOST") or config_dict["db_host"],
+        db_password=os.getenv("DATABASE_PASSWORD") or config_dict["db_password"],
+        db_user=os.getenv("DATABASE_USER") or config_dict["db_user"],
         token=config_dict["token"],
         master_admin_user_id=config_dict["master_admin_user_id"],
     )
-
-
-if __name__ == '__main__':
-    print(load_config())

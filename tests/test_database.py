8b52a187500d058dc7b21652e444c7cd5d8e5d2f
Tomi Javanainen <tomi.javanainen@gmail.com>
2023-11-06 20:53:42 +0200

    Add test for not creating the db when it exists

--- tests/test_database.py
+++ tests/test_database.py
@@ -15,7 +15,7 @@ class TestDatabase(unittest.TestCase):
                                         password=os.getenv("TEST_MARIADB_ROOT_PASSWORD"))
         self.test_db_name = "test_polyamoria_suomi_bot_unique_d83k19tv5lg8"
 
-    def test_create_db_if_needed(self):
+    def test_create_db_if_needed_when_no_db(self):
         # Make sure the test db doesn't exist
         with self.pool.get_connection() as conn:
             cursor = conn.cursor()
@@ -23,7 +23,26 @@ class TestDatabase(unittest.TestCase):
 
         create_db_if_needed(pool=self.pool, db_name=self.test_db_name)
 
-        # Now connect to the test database and check if the new database was created
+        db_names = self._get_databases()
+        self.assertTrue(self.test_db_name in db_names)
+
+    def test_create_db_if_needed_when_already_exists(self):
+        # Create the test db
+        with self.pool.get_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute(f"CREATE DATABASE IF NOT EXISTS {self.test_db_name}")
+
+        create_db_if_needed(pool=self.pool, db_name=self.test_db_name)
+
+        db_names = self._get_databases()
+        self.assertTrue(self.test_db_name in db_names)
+
+    def tearDown(self):
+        with self.pool.get_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute(f"DROP DATABASE IF EXISTS {self.test_db_name}")
+
+    def _get_databases(self) -> list[str]:
         with self.pool.get_connection() as conn:
             cursor = conn.cursor()
             cursor.execute("SHOW DATABASES")
@@ -31,14 +50,7 @@ class TestDatabase(unittest.TestCase):
             # List of database names
             databases = cursor.fetchall()
             db_names = [db[0] for db in databases]
-
-        # Assert that the new test database is in the list of databases
-        self.assertTrue(self.test_db_name in db_names)
-
-    def tearDown(self):
-        with self.pool.get_connection() as conn:
-            cursor = conn.cursor()
-            cursor.execute(f"DROP DATABASE IF EXISTS {self.test_db_name}")
+        return db_names
 
 
 if __name__ == '__main__':

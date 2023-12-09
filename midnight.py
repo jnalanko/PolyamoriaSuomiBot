9556cdd4eb2c19a6a09b27f8dbaea70ca4df9af0
Pupu Toivonen <pupu@pupu.me>
2023-12-10 00:58:31 +0200

    Update midnight.py
    Very liberally accept phrases as a happy midnight greeting.

--- midnight.py
+++ midnight.py
@@ -1,5 +1,4 @@
+import re
+
 def contains_midnight_phrase(text: str) -> bool:
-    # Use regexp for matching "hyv*ke[ks|sk]iyö*"?
-    midnight_strings = ["happy midnight", "hyvää keskiyötä", "hyvää keksiyötä"]
-    text_lower = text.lower()
-    return any((s in text_lower for s in midnight_strings))
+    return bool(re.search('(h.*i.*n.*)|(h.*k.*t.*)', text, re.IGNORECASE))

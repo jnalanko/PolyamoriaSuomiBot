8a19bb6f3837a7605b1db061ea720d8718bfada3
Pupu Toivonen <pupu@pupu.me>
2023-12-10 05:57:30 +0200

    Allow some wiggle in accepted happy midnights
    First check with a regex if this might be a happy midnight expression. Then compute the edit distance to two known expressions and if one of them is close enough, allow it.
    
    This means that small amounts of self expression (e.g. "hyvää keksiyötä"), a couple of small typos or a missing space should not keep anyone from catching the trophy. More than three characters difference means the message will not be considered a midnight phrase.
    
    The regex part isolates the assumed happy midnight string to allow other stuff, such as emojis, to be added before and after the expression without being counted towards the edit distance.
    
    This check requires more cycles than a simple substring match or a simple regex check, so this is better put behind a time barrier, i.e. only do this close to midnight.

--- midnight.py
+++ midnight.py
@@ -1,4 +1,31 @@
 import re
 
 def contains_midnight_phrase(text: str) -> bool:
-    return bool(re.search('(h.*i.*n.*)|(h.*k.*t.*)', text, re.IGNORECASE))
+	match = re.search('(h.*?i.*?n.*?t)|(h.*?k.*?t.*?ä)', text, re.IGNORECASE)
+	if not match:
+		return False
+	n_edits = min(
+		distance(match.group(), "happy midnight"),
+		distance(match.group(), "hyvää keskiyötä")
+		)
+	return n_edits <= 3
+
+def distance(t1:str, t2:str):
+	return caching_levenshtein(t1.lower(), t2.lower())
+
+def caching_levenshtein(t1:str, t2:str, cache:dict = {}):
+	if len(t1) == 0:
+		return len(t2)
+	if len(t2) == 0:
+		return len(t1)
+	if (t1,t2) in cache:
+		return cache[(t1,t2)]
+	if (t1[0] == t2[0]):
+		cache[(t1,t2)] = caching_levenshtein(t1[1:], t2[1:], cache)
+		return cache[(t1,t2)]
+	cache[(t1,t2)] = 1 + min(
+		caching_levenshtein(t1[1:], t2,     cache),
+		caching_levenshtein(t1,     t2[1:], cache),
+		caching_levenshtein(t1[1:], t2[1:], cache)
+		)
+	return cache[(t1,t2)]

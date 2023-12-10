d671fa8753b87395a5cb544dfa8ad4cf6c243c93
Pupu Toivonen <pupu@pupu.me>
2023-12-10 15:48:46 +0200

    Add function to decide which trophy to give
    Quick match to find a substring that maybe could look like the happy midnight phrase, then see if it contains a cookie.
    
    Default to trophy.

--- midnight.py
+++ midnight.py
@@ -1,5 +1,16 @@
+import re
+
 def contains_midnight_phrase(text: str) -> bool:
     # Use regexp for matching "hyv*ke[ks|sk]iyö*"?
     midnight_strings = ["happy midnight", "hyvää keskiyötä", "hyvää keksiyötä"]
     text_lower = text.lower()
     return any((s in text_lower for s in midnight_strings))
+
+
+
+def message_to_trophy(msg:str) -> str:
+    match = re.search('(h.*?i.*?n.*?t)|(h.*?k.*?t.*?ä)', msg, re.IGNORECASE)
+    matchtext = match.group()
+    if bool(re.search('keksi', matchtext, re.IGNORECASE)):
+        return '🍪'
+    return '🏆'
